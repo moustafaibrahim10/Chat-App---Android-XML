72d6b951859d6b9e1000e35eacab142858abb2de
moustafaibrahim10 <moustafaibrahim.dev@gmail.com>
2025-09-13 07:02:51 +0300

    splash

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.chatapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler().postDelayed({
+            val  intent = Intent(this,Onboarding :: class.java)
+            startActivity(intent)
+            finish()
+
+        },
+            3000
+        )
+
     }
 }
+
+
+
+/*
+Handler().postDelayed({
+            // Create an Intent to open the next Activity
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+
+            // Finish the current Activity so the user can't go back to the splash screen
+            finish()
+        }, 3000)
+ */
