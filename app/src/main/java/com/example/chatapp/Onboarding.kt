a5fdb960e2514c3411342d7527c5c509b1551902
moustafaibrahim10 <moustafaibrahim.dev@gmail.com>
2025-09-22 10:02:52 +0300

    end onboarding

--- app/src/main/java/com/example/chatapp/Onboarding.kt
+++ app/src/main/java/com/example/chatapp/Onboarding.kt
@@ -1,6 +1,8 @@
 package com.example.chatapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,10 @@ class Onboarding : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.onboarding)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+       val btn = findViewById<TextView>(R.id.logIn)
+        val intent = Intent(this, SignIn::class.java)
+        btn.setOnClickListener {
+            startActivity(intent)
         }
     }
 }
